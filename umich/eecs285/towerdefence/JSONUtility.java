145837950e26ae095fadc356c03ace1408445af8
luo19930414 <luo19930414@gmail.com>
2013-11-24 23:07:26 -0500

    Update JSONUtility

--- umich/eecs285/towerdefence/JSONUtility.java
+++ umich/eecs285/towerdefence/JSONUtility.java
@@ -1,25 +1,54 @@
 package umich.eecs285.towerdefence;
-
+/**
+ * @author Jiaxi Luo
+ * This is a utility class providing method to transfer TowerDefense_TransData to JSON or vice versa.
+ */
+import java.io.IOException;
 import java.sql.Timestamp;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-public final class JSONUtility implements TowerDefensedataArray{
-  String arrayToJSON(int clientId, TowerDefense_TransData towerDefense_TransData) {
+import umich.eecs285.towerdefence.TowerDefensedataArray.TowerDefenseObject;
+import umich.eecs285.towerdefence.TowerDefensedataArray.TowerDefense_TransData;
+
+public final class JSONUtility {
+  
+  @SuppressWarnings("unchecked")
+  public String arrayToJSON(int clientId, TowerDefense_TransData towerDefense_TransData) throws IOException {
     JSONObject obj = new JSONObject();
     JSONArray arrayObj = new JSONArray();
+    obj.put("clientId", clientId);
+    obj.put("timestamp", new Timestamp(System.currentTimeMillis()));
+    for(int i = 0; i < TowerDefensedataArray.TowerDefenseObject_Array_Size; i++) {
+      JSONObject arrayData = new JSONObject();
+      arrayData.put("id", TowerDefense_TransData.TowerDefense_TransArray[i].getId());
+      arrayData.put("life", TowerDefense_TransData.TowerDefense_TransArray[i].getLife());
+      arrayData.put("x", TowerDefense_TransData.TowerDefense_TransArray[i].getX());
+      arrayData.put("y", TowerDefense_TransData.TowerDefense_TransArray[i].getY());
+      arrayData.put("action", TowerDefense_TransData.TowerDefense_TransArray[i].getAction());
+      arrayObj.add(arrayData);
+    }
+    obj.put("arrayObj", arrayObj);
+    return obj.toString();
+  }
+  
+  public TowerDefense_TransData JOSNToArray(String jsonString) {
+    JSONParser parser = new JSONParser();
+    TowerDefense_TransData towerDefense_TransData = new TowerDefense_TransData();
     try {
-      obj.put("clientId", clientId);
-      obj.put("timestamp", new Timestamp(System.currentTimeMillis()));
-      for(int i = 0; i < TowerDefenseObject_Array_Size; i++) {
-        JSONObject arrayData = new JSONObject();
-        arrayData.put("id", TowerDefense_TransData.TowerDefense_TransArray[i].getId());
-        arrayObj.put(arrayData);
+      JSONObject jsonObject = (JSONObject) parser.parse(jsonString);
+      for (int i = 0; i < TowerDefensedataArray.TowerDefenseObject_Array_Size; i++) {
+        TowerDefense_TransData.TowerDefense_TransArray[i] = new TowerDefenseObject();
+//        int id = (Integer) jsonObject.get("id");
+//        int life = (Integer) jsonObject.get("life");
+//        int x = (Integer) jsonObject.get("x");
+//        int y = (Integer) jsonObject.get("y");
+//        int action = (Integer) jsonObject.get("action");
       }
-      obj.put("arrayObj", arrayObj);
-    } catch (JSONException e) {
+    } catch (ParseException e) {
       e.printStackTrace();
     }
     return null;
--- umich/eecs285/towerdefence/TowerDefensedataArray_Example_Usage.java
+++ umich/eecs285/towerdefence/TowerDefensedataArray_Example_Usage.java
@@ -3,13 +3,13 @@ package umich.eecs285.towerdefence;
 /*
  * Example of use
  */
-public class  TowerDefensedataArray_Example_Usage implements TowerDefensedataArray{
-   
+public class  TowerDefensedataArray_Example_Usage implements TowerDefensedataArray {
+  public static final int TowerDefenseObject_Array_Size = 30;
+  
   public static void main(String[] args) {
     TowerDefense_TransData t = new TowerDefense_TransData();
-    for(int i=0;i<TowerDefenseObject_Array_Size;i++)
-      TowerDefense_TransData.TowerDefense_TransArray[i]=new TowerDefenseObject();
+    for(int i = 0; i < TowerDefenseObject_Array_Size; i++)
+      TowerDefense_TransData.TowerDefense_TransArray[i] = new TowerDefenseObject();
     System.out.println(t.toString());
-    
   }
 }
