3afbb0ddc62571297901d44c9828d146d8313406
luo19930414 <luo19930414@gmail.com>
2013-11-25 20:52:01 -0500

    Complete ServerMessager

--- umich/eecs285/towerdefence/ServerMessager.java
+++ umich/eecs285/towerdefence/ServerMessager.java
@@ -11,6 +11,7 @@ public class ServerMessager extends Thread {
   private ServerSocket serverSocket;
   
   public ServerMessager(int port) throws IOException {
+    // TODO figure out proper server settings
     serverSocket = new ServerSocket(port, 100000);
     serverSocket.setSoTimeout(1000);
     
@@ -23,22 +24,38 @@ public class ServerMessager extends Thread {
   public void run() {
     while(true) {
     try {
+      // initialize server
       InetAddress hostIp;
       hostIp = InetAddress.getLocalHost();
       System.out.println("Waiting for client on " + hostIp.getHostAddress() + ":" +
       serverSocket.getLocalPort() + "...");
+      
+      // setup connection
       Socket server = serverSocket.accept();
+      
+      // get connection
       System.out.println("Just connected to "
            + server.getRemoteSocketAddress());
+      
+      // get input message
       DataInputStream in =
            new DataInputStream(server.getInputStream());
-      System.out.println(in.readUTF());
+      TowerDefense_TransData towerDefense_TransData = JSONUtility.JOSNToArray(in.readUTF());
+      clientMessageBufferList[towerDefense_TransData.getId()] = towerDefense_TransData;
+      // TODO need to change for more than two players
      
+      // send out output message
+      int sendId;
+      if (towerDefense_TransData.getId() == 1)
+        sendId = 0;
+      else
+        sendId = 1;
       DataOutputStream out =
           new DataOutputStream(server.getOutputStream());
-      out.writeUTF("Thank you for connecting to "
-                  + server.getLocalSocketAddress() + "\nGoodbye!");
+      out.writeUTF(JSONUtility.arrayToJSON(clientMessageBufferList[sendId]));
+      clientMessageBufferList[sendId] = TowerDefense_TransData.createEmptyTransData(sendId);
        
+      // close server
       server.close();
     } catch(SocketTimeoutException s) {
         System.out.println("Socket timed out!");
