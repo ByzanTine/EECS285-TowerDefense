7bfac542ecf2dc951b59f3cd4c0e6a57bfe307b5
ByzanTine <morbyzantine@gmail.com>
2013-11-27 17:14:54 -0500

    DataBase update

--- src/umich/eecs285/towerdefence/TowerDefenseDataBase.java
+++ src/umich/eecs285/towerdefence/TowerDefenseDataBase.java
@@ -1,30 +1,37 @@
 package umich.eecs285.towerdefence;
+
 import java.util.Hashtable;
 
 public class TowerDefenseDataBase {
-	private static final int ID_SIZE=100;
-	private static final int ACTION_SIZE=50;
+	private static final int ID_SIZE = 100;
+	private static final int ACTION_SIZE = 50;
 	Hashtable<Integer, Hashtable<Integer, String>> image_table;
+	Hashtable<Integer, Units> Unit_table;
 
 	public TowerDefenseDataBase() {
 		image_table = new Hashtable<Integer, Hashtable<Integer, String>>();
-
+		Unit_table = new Hashtable<Integer, Units>();
 	}
 
 	/**
 	 * Init the table with filenames
 	 */
 	public void init() {
-		for(int i=0;i<ID_SIZE;i++){	
-			for(int j=0;j<ACTION_SIZE;j++){
-				String filename = "";
-				filename+=i;
-				filename+='_';
-				filename+=j;
-				
+		for (int i = 0; i < ID_SIZE; i++) {
+			for (int j = 0; j < ACTION_SIZE; j++) {
+				String filename = "id";
+				filename += i;
+				filename += '/';
+				filename += j;
+
 				this.insert(i, j, filename);
 			}
 		}
+		for (int i = 0; i < ID_SIZE; i++) {
+			// Units unit=new Units(1,1,1,1,1,1);
+
+		}
+
 	}
 
 	private void insert(int id, int action, String filename) {
@@ -38,14 +45,35 @@ public class TowerDefenseDataBase {
 		actionTable.put(action, filename);
 		image_table.put(id, actionTable);
 	}
+
 	/**
 	 * Obtain a filename string by id and action
+	 * 
 	 * @param id
 	 * @param action
-	 * @return
+	 * @return filename
 	 */
-	public String search(int id, int action) {
-		return image_table.get(id).get(action);
+	public String searchImage(int rawId, int action) {
+		return image_table.get(this.processID(rawId)).get(action);
+	}
+
+	/**
+	 * Obtain a pre-defined Unit by id
+	 * 
+	 * @param id
+	 * @return Unit
+	 */
+	public Units search(int rawId) {
+		return Unit_table.get(this.processID(rawId));
+	}
+
+	/**
+	 * 
+	 * @param rawId
+	 * @return processedId
+	 */
+	private int processID(int rawId) {
+		return rawId % ID_SIZE;
 	}
 
 }
