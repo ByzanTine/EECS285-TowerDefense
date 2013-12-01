13160a7f73cd990da0158b0f1564a3269fefa641
luo19930414 <luo19930414@gmail.com>
2013-11-30 22:00:11 -0500

    refract main

--- src/umich/eecs285/towerdefence/TowerDefenseGame.java
+++ src/umich/eecs285/towerdefence/TowerDefenseGame.java
@@ -5,172 +5,172 @@ import java.util.Timer;
 import javax.swing.JFrame;
 
 public class TowerDefenseGame implements TowerDefensedataArray {
-	private static Controller control;
-	private static TowerDefenseDataBase DB;
-	private static ClientBridge client_bridge;
-	private static MainFrame mainFrame;
-	private static Player player;
-	private static int turn;
-	private static long timestamp;
-	// Increase Automatically
-	private static int clientId = 0;
+  private static Controller control;
+  private static TowerDefenseDataBase DB;
+  private static ClientBridge client_bridge;
+  private static MainFrame mainFrame;
+  private static Player player;
+  private static int turn;
+  private static long timestamp;
+  // Increase Automatically
+  private static int clientId = 0;
 
-	private void init() {
+  private void init() {
 
-		mainFrame = new MainFrame();
-		mainFrame.setVisible(true);
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		player = new Player();
-		turn = 1;
-		control = new Controller();
-		control.init();
-		DB = new TowerDefenseDataBase();
-		DB.init();
-		client_bridge = new ClientBridge();
-		mainFrame.setClientBridge(client_bridge);
-	}
+    mainFrame = new MainFrame();
+    mainFrame.setVisible(true);
+    mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    player = new Player();
+    turn = 1;
+    control = new Controller();
+    control.init();
+    DB = new TowerDefenseDataBase();
+    DB.init();
+    client_bridge = new ClientBridge();
+    mainFrame.setClientBridge(client_bridge);
+  }
 
-	private void setTimestamp() {
-		timestamp = System.currentTimeMillis();
-	}
+  private void setTimestamp() {
+    timestamp = System.currentTimeMillis();
+  }
 
-	public static void main(String args[]) {
-		TowerDefenseGame game = new TowerDefenseGame();
-		game.init();
-		// Round 1
+  public static void main(String args[]) {
+    TowerDefenseGame game = new TowerDefenseGame();
+    game.init();
+    // Round 1
 
-		TowerDefense_TransData data;
-		game.setTimestamp();
-		// paint data
-		// Prep
-		long prep = timestamp;
-		long begin = timestamp;
-		while (true) {
-			game.setTimestamp();
-			if (prep + 50 <= timestamp) {
-				// Look up Bridge
-				prep = timestamp;
-				game.checkprepBridge();
-				data = control.getInfo(clientId, timestamp);
-				// paint data
-				System.out.println(data.toString());
+    TowerDefense_TransData data;
+    game.setTimestamp();
+    // paint data
+    // Prep
+    long prep = timestamp;
+    long begin = timestamp;
+    while (true) {
+      game.setTimestamp();
+      if (prep + 50 <= timestamp) {
+        // Look up Bridge
+        prep = timestamp;
+        game.checkprepBridge();
+        data = control.getInfo(clientId, timestamp);
+        // paint data
+        System.out.println(data.toString());
 
-				mainFrame.nextFrame(data);
-			}
-			if (begin + 10000 <= timestamp) {
-				// Time control will be determined by the Timer
-				break;
-			}
+        mainFrame.nextFrame(data);
+      }
+      if (begin + 10000 <= timestamp) {
+        // Time control will be determined by the Timer
+        break;
+      }
 
-		}
-		// Running
-		int[] attackingUnits = player.getAttackingId();
-		control.startTurn(turn, attackingUnits.length, attackingUnits);
-		long run = timestamp;
-		while (!control.isEnd()) {
-			game.setTimestamp();
-			if (run + 50 <= timestamp) {
-				run = timestamp;
-				game.checkRunBridge();
-				control.run();
-				data = control.getInfo(clientId, timestamp);
-				// paint
+    }
+    // Running
+    int[] attackingUnits = player.getAttackingId();
+    control.startTurn(turn, attackingUnits.length, attackingUnits);
+    long run = timestamp;
+    while (!control.isEnd()) {
+      game.setTimestamp();
+      if (run + 50 <= timestamp) {
+        run = timestamp;
+        game.checkRunBridge();
+        control.run();
+        data = control.getInfo(clientId, timestamp);
+        // paint
 
-				try {
-					mainFrame.nextFrame(data);
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
+        try {
+          mainFrame.nextFrame(data);
+        } catch (Exception e) {
+          // TODO Auto-generated catch block
+          e.printStackTrace();
+        }
 
-				System.out.println(data.toString());
-				if (control.isDead()) {
-					for (int i = 0; i < 3000; i++) {
+        System.out.println(data.toString());
+        if (control.isDead()) {
+          for (int i = 0; i < 3000; i++) {
 
-						control.run();
-						data = control.getInfo(clientId, timestamp);
-						// paint
-						mainFrame.nextFrame(data);
-					}
-					break;
-				}
+            control.run();
+            data = control.getInfo(clientId, timestamp);
+            // paint
+            mainFrame.nextFrame(data);
+          }
+          break;
+        }
 
-			}
-			// System.out.println(timestamp);
-			// if (control.isDead()) {
-			// break;
-			// }
-		}
-		for (int i = 0; i < 3000; i++) {
+      }
+      // System.out.println(timestamp);
+      // if (control.isDead()) {
+      // break;
+      // }
+    }
+    for (int i = 0; i < 3000; i++) {
 
-			control.run();
-			data = control.getInfo(clientId, timestamp);
-			// paint
-			mainFrame.nextFrame(data);
-		}
-		System.out.print("End Round");
-		control.endTurn();
-		player.addCandy(1, control.hasReachedKing());
-		// TODO player automatically increase money
-	}
+      control.run();
+      data = control.getInfo(clientId, timestamp);
+      // paint
+      mainFrame.nextFrame(data);
+    }
+    System.out.print("End Round");
+    control.endTurn();
+    player.addCandy(1, control.hasReachedKing());
+    // TODO player automatically increase money
+  }
 
-	private void checkprepBridge() {
+  private void checkprepBridge() {
 
-		// TODO Auto-generated method stub
-		if (client_bridge.isCreateAttackUnitRequest()) {
-			if (player.canCreateAttackingUnit(client_bridge.getAttackUnitId())) {
-				player.createAttackingUnit(client_bridge.getAttackUnitId());
+    // TODO Auto-generated method stub
+    if (client_bridge.isCreateAttackUnitRequest()) {
+      if (player.canCreateAttackingUnit(client_bridge.getAttackUnitId())) {
+        player.createAttackingUnit(client_bridge.getAttackUnitId());
 
-			}
-		}
-		if (client_bridge.isCreateUnitRequest()) {
+      }
+    }
+    if (client_bridge.isCreateUnitRequest()) {
 
-			if (player.canCreateUnit(client_bridge.getId())) {
-				System.out.println("Can Create ");
-				if (control.addUnit(client_bridge.getId(),
-						client_bridge.getX(), client_bridge.getY(), 1)) {
+      if (player.canCreateUnit(client_bridge.getId())) {
+        System.out.println("Can Create ");
+        if (control.addUnit(client_bridge.getId(), client_bridge.getX(),
+            client_bridge.getY(), 1)) {
 
-					player.createUnit(client_bridge.getId());
-				}
-			}
-			client_bridge.setCreateUnitRequest(false);
-		}
-		if (client_bridge.isMeoMeoNumIncreaseRequest()) {
-			if (player.canCreateMeoMeo()) {
-				player.createMeoMeo();
-			}
-		}
-		if (client_bridge.isMeoMeoTechUpgradeRequest()) {
-			if (player.canUpdateMeoMeo()) {
-				player.updateMeoMeo();
-			}
-		}
-		if (client_bridge.isUnitLevelupRequest()) {
-			if (player.canUpdateUnit(client_bridge.getLevelupId())) {
-				player.updateUnit(client_bridge.getLevelupId());
-				control.levelUp(client_bridge.getLevelupId());
-			}
-		}
-	}
+          player.createUnit(client_bridge.getId());
+        }
+      }
+      client_bridge.setCreateUnitRequest(false);
+    }
+    if (client_bridge.isMeoMeoNumIncreaseRequest()) {
+      if (player.canCreateMeoMeo()) {
+        player.createMeoMeo();
+      }
+    }
+    if (client_bridge.isMeoMeoTechUpgradeRequest()) {
+      if (player.canUpdateMeoMeo()) {
+        player.updateMeoMeo();
+      }
+    }
+    if (client_bridge.isUnitLevelupRequest()) {
+      if (player.canUpdateUnit(client_bridge.getLevelupId())) {
+        player.updateUnit(client_bridge.getLevelupId());
+        control.levelUp(client_bridge.getLevelupId());
+      }
+    }
+  }
 
-	private void checkRunBridge() {
-		// TODO Auto-generated method stub
-		if (client_bridge.isCreateAttackUnitRequest()) {
-			if (player.canCreateAttackingUnit(client_bridge.getAttackUnitId())) {
-				player.createAttackingUnit(client_bridge.getAttackUnitId());
+  private void checkRunBridge() {
+    // TODO Auto-generated method stub
+    if (client_bridge.isCreateAttackUnitRequest()) {
+      if (player.canCreateAttackingUnit(client_bridge.getAttackUnitId())) {
+        player.createAttackingUnit(client_bridge.getAttackUnitId());
 
-			}
-		}
-		if (client_bridge.isMeoMeoNumIncreaseRequest()) {
-			if (player.canCreateMeoMeo()) {
-				player.createMeoMeo();
-			}
-		}
-		if (client_bridge.isMeoMeoTechUpgradeRequest()) {
-			if (player.canUpdateMeoMeo()) {
-				player.updateMeoMeo();
-			}
-		}
+      }
+    }
+    if (client_bridge.isMeoMeoNumIncreaseRequest()) {
+      if (player.canCreateMeoMeo()) {
+        player.createMeoMeo();
+      }
+    }
+    if (client_bridge.isMeoMeoTechUpgradeRequest()) {
+      if (player.canUpdateMeoMeo()) {
+        player.updateMeoMeo();
+      }
+    }
 
-	}
+  }
 }
